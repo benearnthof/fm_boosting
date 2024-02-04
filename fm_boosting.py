507d3414007de463e5eaff9b2683036b295ec474
benearnthof <benearnthof@hotmail.de>
2024-02-05 00:20:56 +0100

    remove unet wrapper from fm_boosting.py

--- fm_boosting.py
+++ fm_boosting.py
@@ -138,83 +138,8 @@ def infiniteloop(dataloader):
         for x in iter(dataloader):
             yield x
 
-# define slightly other wrapper since we need a specific number of output channels so we can 
-# concatenate noise to the upsampled low dim latents and obtain a 1:1 mapping to the high res latents
-# NUM_CLASSES = 1000
-
-# class UNetModelWrapper(UNetModel):
-#     def __init__(
-#         self,
-#         dim,
-#         num_channels,
-#         num_res_blocks,
-#         channel_mult=None,
-#         learn_sigma=False,
-#         class_cond=False,
-#         num_classes=NUM_CLASSES,
-#         use_checkpoint=False,
-#         attention_resolutions="16",
-#         num_heads=1,
-#         num_head_channels=-1,
-#         num_heads_upsample=-1,
-#         use_scale_shift_norm=False,
-#         dropout=0,
-#         resblock_updown=False,
-#         use_fp16=False,
-#         use_new_attention_order=False,
-#     ):
-#         """Dim (tuple): (C, H, W)"""
-#         image_size = dim[-1]
-#         if channel_mult is None:
-#             if image_size == 512:
-#                 channel_mult = (0.5, 1, 1, 2, 2, 4, 4)
-#             elif image_size == 256:
-#                 channel_mult = (1, 1, 2, 2, 4, 4)
-#             elif image_size == 128:
-#                 channel_mult = (1, 1, 2, 3, 4)
-#             elif image_size == 64:
-#                 channel_mult = (1, 2, 3, 4)
-#             elif image_size == 32:
-#                 channel_mult = (1, 2, 2, 2)
-#             elif image_size == 28:
-#                 channel_mult = (1, 2, 2)
-#             else:
-#                 raise ValueError(f"unsupported image size: {image_size}")
-#         else:
-#             channel_mult = list(channel_mult)
-#         attention_ds = []
-#         for res in attention_resolutions.split(","):
-#             attention_ds.append(image_size // int(res))
-#         return super().__init__(
-#             image_size=image_size,
-#             in_channels=dim[0],
-#             model_channels=num_channels,
-#             # need to hardcode out_channels as dim[0] / 2 since we map from 
-#             # (b, 8, 128, 128) to (b, 4, 128, 128)
-#             out_channels=4,
-#             num_res_blocks=num_res_blocks,
-#             attention_resolutions=tuple(attention_ds),
-#             dropout=dropout,
-#             channel_mult=channel_mult,
-#             num_classes=(num_classes if class_cond else None),
-#             use_checkpoint=use_checkpoint,
-#             use_fp16=use_fp16,
-#             num_heads=num_heads,
-#             num_head_channels=num_head_channels,
-#             num_heads_upsample=num_heads_upsample,
-#             use_scale_shift_norm=use_scale_shift_norm,
-#             resblock_updown=resblock_updown,
-#             use_new_attention_order=use_new_attention_order,
-#         )
-#     def forward(self, t, x, y=None, *args, **kwargs):
-#         return super().forward(t, x, y=y)
-
-
-
 
 #### Flow Matching
-
-
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("data_path", "/dss/dssmcmlfs01/pr74ze/pr74ze-dss-0001/ru25jan4/data/CelebA/celeba/img_align_celeba", help="path to CelebA directory")
