16e2adf13f1cc73dae5acf1740f5b113102f6bc1
benearnthof <benearnthof@hotmail.de>
2024-03-08 23:00:49 +0100

    swap unet to support conditioning

--- fm_boosting.py
+++ fm_boosting.py
@@ -31,6 +31,8 @@ from torchcfm.conditional_flow_matching import (
 )
 from torchcfm.models.unet.unet import UNetModel, UNetModelWrapper
 
+from imagen_pytorch import Unet
+
 #### Setup
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model_id = "runwayml/stable-diffusion-v1-5"
@@ -140,76 +142,6 @@ def infiniteloop(dataloader):
 
 # define slightly other wrapper since we need a specific number of output channels so we can 
 # concatenate noise to the upsampled low dim latents and obtain a 1:1 mapping to the high res latents
-NUM_CLASSES = 1000
-
-class UNetModelWrapper(UNetModel):
-    def __init__(
-        self,
-        dim,
-        num_channels,
-        num_res_blocks,
-        channel_mult=None,
-        learn_sigma=False,
-        class_cond=False,
-        num_classes=NUM_CLASSES,
-        use_checkpoint=False,
-        attention_resolutions="16",
-        num_heads=1,
-        num_head_channels=-1,
-        num_heads_upsample=-1,
-        use_scale_shift_norm=False,
-        dropout=0,
-        resblock_updown=False,
-        use_fp16=False,
-        use_new_attention_order=False,
-    ):
-        """Dim (tuple): (C, H, W)"""
-        image_size = dim[-1]
-        if channel_mult is None:
-            if image_size == 512:
-                channel_mult = (0.5, 1, 1, 2, 2, 4, 4)
-            elif image_size == 256:
-                channel_mult = (1, 1, 2, 2, 4, 4)
-            elif image_size == 128:
-                channel_mult = (1, 1, 2, 3, 4)
-            elif image_size == 64:
-                channel_mult = (1, 2, 3, 4)
-            elif image_size == 32:
-                channel_mult = (1, 2, 2, 2)
-            elif image_size == 28:
-                channel_mult = (1, 2, 2)
-            else:
-                raise ValueError(f"unsupported image size: {image_size}")
-        else:
-            channel_mult = list(channel_mult)
-        attention_ds = []
-        for res in attention_resolutions.split(","):
-            attention_ds.append(image_size // int(res))
-        return super().__init__(
-            image_size=image_size,
-            in_channels=dim[0],
-            model_channels=num_channels,
-            # need to hardcode out_channels as dim[0] / 2 since we map from 
-            # (b, 8, 128, 128) to (b, 4, 128, 128)
-            out_channels=4,
-            num_res_blocks=num_res_blocks,
-            attention_resolutions=tuple(attention_ds),
-            dropout=dropout,
-            channel_mult=channel_mult,
-            num_classes=(num_classes if class_cond else None),
-            use_checkpoint=use_checkpoint,
-            use_fp16=use_fp16,
-            num_heads=num_heads,
-            num_head_channels=num_head_channels,
-            num_heads_upsample=num_heads_upsample,
-            use_scale_shift_norm=use_scale_shift_norm,
-            resblock_updown=resblock_updown,
-            use_new_attention_order=use_new_attention_order,
-        )
-    def forward(self, t, x, y=None, *args, **kwargs):
-        return super().forward(t, x, y=y)
-
-
 
 
 #### Flow Matching
@@ -262,7 +194,7 @@ def train(argv):
     # DATASETS/DATALOADER
     celeba_path = Path(FLAGS.data_path)
     assert celeba_path.exists()
-    dataset = ImageDataset(folder=celeba_path, image_size=1024)
+    dataset = ImageDataset(folder=celeba_path, image_size=256)
 
     dataloader = torch.utils.data.DataLoader(
         dataset,
@@ -276,18 +208,31 @@ def train(argv):
     
 
     # MODELS
-    net_model = UNetModelWrapper(
-        dim=(4, 128, 128), # must match channel, h, w of hi res latent
-        num_res_blocks=3,               # assuming this is "Depth" in table 8 https://arxiv.org/pdf/2312.07360.pdf#table.caption.28
-        num_channels=FLAGS.num_channel, # 128 like in table 8
-        channel_mult=[1, 2, 3, 4],      # channel mult from table 8 FacesHQ
-        num_heads=4,                # Number of Heads table 8
-        num_head_channels=64,       # Head channels table 8
-        attention_resolutions="16", # Attention resolutions also table 8
-        dropout=0.1,                # added dropout for a bit of regularization
-    ).to(
-        device
-    )  # new dropout + bs of 128
+    # net_model = UNetModelWrapper(
+    #     dim=(4, 128, 128), # must match channel, h, w of hi res latent
+    #     num_res_blocks=3,               # assuming this is "Depth" in table 8 https://arxiv.org/pdf/2312.07360.pdf#table.caption.28
+    #     num_classes=1024,               # 4*16*16 for upsampling from 128 to 256
+    #     class_cond=True,
+    #     num_channels=FLAGS.num_channel, # 128 like in table 8
+    #     channel_mult=[1, 2, 3, 4],      # channel mult from table 8 FacesHQ
+    #     num_heads=4,                # Number of Heads table 8
+    #     num_head_channels=64,       # Head channels table 8
+    #     attention_resolutions="16", # Attention resolutions also table 8
+    #     dropout=0.1,                # added dropout for a bit of regularization
+    # ).to(
+    #     device
+    # )  # new dropout + bs of 128
+    
+    # new unet for conditional modeling of low res latents
+    net_model = Unet(
+        dim = 128,
+        channels=4,
+        cond_images_channels = 4,
+        dim_mults = (1, 2, 4, 8),
+        num_resnet_blocks = (2, 4, 8, 8),
+        layer_attns = (False, False, False, True),
+        layer_cross_attns = (False, False, False, True)
+    ).to(device)
 
     ema_model = copy.deepcopy(net_model)
     optim = torch.optim.Adam(net_model.parameters(), lr=FLAGS.lr)
@@ -330,109 +275,33 @@ def train(argv):
         for step in pbar:
             optim.zero_grad()
             hi_res_images = next(datalooper).to(device)
-            lo_res_images = downsample_2d(hi_res_images.cpu(), sz=256).to(device)
+            lo_res_images = downsample_2d(hi_res_images.cpu(), sz=128).to(device)
             with torch.no_grad():
                 hi_res_latents = vae.encode(hi_res_images).latent_dist.sample()
                 lo_res_latents = vae.encode(lo_res_images).latent_dist.sample()
-            # hi_res_latent_shape: torch.Size([1, 4, 128, 128])
-            # lo_res_latent_shape: torch.Size([1, 4, 32, 32])
-            # we naively upsample the low res latent to 4x128x128
-            # then add noise to this
-            # then perform flow matching from noised latent to original latent
-            # then decode the flow matched latent with the vae decoder
-            # naively upsample the low resolution latents
-            with torch.no_grad():
-                # output size must match last 2 spatial dimensions of hi res latents
-                bilinear_latents = F.interpolate(lo_res_latents, size=hi_res_latents.shape[-2:], mode="nearest")
-            # concatenate noise to bilinear latents
-            # TODO: Here I just add noise to the tensor since it would lead to a shape mismatch 
-            # during sampling location and conditional flow.
-            # TODO: Need reference implementation for this since this shape mismatch is also 
-            # present in figure 3 but is not explained.
-            # Noise Augmentation 
-            # https://arxiv.org/abs/2210.02303
-            # https://arxiv.org/abs/2205.11487
-            # https://github.com/CompVis/fm-boosting/issues/
-            # TODO: investigate impact of noise scale
-            # calculate stds with cosine schedule: 
-            # sample t first
-            t, xt, ut = FM.sample_location_and_conditional_flow(bilinear_latents, hi_res_latents)
-            augment_means = torch.zeros_like(bilinear_latents)
-            stds = 1 - torch.cos((t + 0.008) / 1.008 * torch.pi / 2) ** 2
-            std_helper = []
-            for std in stds:
-                std_helper.append(torch.full_like(torch.Tensor(augment_means.shape[1:]), std))
-            std_tensors = torch.stack(std_helper).cuda()
-            augment_noise = torch.normal(augment_means, std=std_tensors)
-            # I will just add some noise with the minimum sigma as mentioned in section 8
-            # 1e-4
-            # TODO: Do this according to cosine schedule 3.2.3 Noise Augmentation in paper
-            # cosine schedule: sample t first
-            # t = torch.rand(1).cuda()
-            # import math
-            # std = 1 - math.cos((t + 0.008) / 1.008 * math.pi / 2) ** 2
-            # means = torch.zeros_like(bilinear_latents)
-            # noise = torch.normal(means, std=std)
-            # noise = torch.randn_like(bilinear_latents, )
-            #### Now we can do the actual flow matching
-            # now we obtain our x1 by just concatenating noise like they did in the paper
-            # target
+            # we do conditional flow matching with the low res latents as conditional info
+            # we flow match from noise to hi res latents with lo res latents as cond info
             x1 = hi_res_latents
-            # source
-            x0 = bilinear_latents + augment_noise
-            
-            # TODO: There has to be a better way of doing this, a custom net? Idk how the authors do it
-            # The main thing i dont get is the fact that the shapes need to match since the flow matching samples are drawn before
-            # we pass the inputs through the unet, but the authors say they use a custom unet that takes these shapes as inputs
-            # so at which time do they perform the flow matching step? 
-            # m1 = torch.nn.Conv3d(8, 4, kernel_size=(3, 1, 1)).cuda()
-            # m2 = torch.nn.Conv3d(4, 4, kernel_size=(3, 1, 1)).cuda()
-            # out = m1(x0)
-            # out = m2(out)
-            # x0 = out
-            # out.shape
+            y = lo_res_latents
+            x0 = torch.randn_like(x1)
+            t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
+            # reshape and concat noise
+            cond_img = y.reshape((8, 1, 32, 32))
+            noise = torch.randn((8, 3, 32, 32)).to(device)
+            cond_img = torch.concatenate([cond_img, noise], dim=1)
 
-            # let x1 denote a high resolution data sample
-            # the conditioning signal z := x1 remains unchanged from the previous formulation
-            # the starting point x0 = Encoder(x1) corresponds to an encoded representation of the image
-            # ut = x1 - x0
-            t_helper = []
-            for ti in t:
-                t_helper.append(torch.full_like(x1[1], ti))
-            t_tensors = torch.stack(t_helper).cuda()
-            # batch wise linear interpolation with t values
-            xt = (t_tensors * x1) + ((1-t_tensors) * x0)
-            # cfm loss: unet(t, xt) - (x1 - x0)
-
-            
-            # xt = samples drawn from probability path pxt at time t
-            # ut = conditional vector field 
-            # concat noise
-            xt_concat = torch.cat((xt, torch.randn_like(xt)), dim=1)
-            # map to target shape
-            vt = net_model(t, xt)
-
-            # we perform flow matching sampling first to get xt, t and ut
-            # then we concat noise to xt to get the expressive power of flow matching from distributions? 
-            # then we pass xt_concat through the unet and do regular flow matching
-
-
-            # Training objective in the paper: 
-            # L2 loss of (net(t, xt) - x1-x0)
-            # The conditioning signal x1 (hi_res_latents) remains unchanged
-            # the starting point x0 correspoinds to an encoded representation of the image
-            # standard flow matching loss
-            # loss = torch.mean((vt - ut) ** 2)
-            # coupling flow matching loss
-            loss = torch.mean((vt - (x1 - x0)) ** 2) #(??)
+            lowres_noise_times = torch.zeros(y.shape[0])
+            vt = net_model(x=xt, time=t, cond_images=cond_img, lowres_noise_times=lowres_noise_times)
+            loss = torch.mean((vt - ut) ** 2)
             loss.backward()
+
             torch.nn.utils.clip_grad_norm_(net_model.parameters(), FLAGS.grad_clip)  # new
             optim.step()
             sched.step()
             ema(net_model, ema_model, FLAGS.ema_decay)  # new
-            print(loss.data.cpu().numpy().tolist())
-            # if step % 1 == 0:
-            #     print(loss.data.cpu().numpy().tolist())
+            # print(loss.data.cpu().numpy().tolist())
+            if step % 10 == 0:
+                print(loss.data.cpu().numpy().tolist())
             # sample and Saving the weights
             if FLAGS.save_step > 0 and step % FLAGS.save_step == 0:
                 # TODO: this does not work yet since we only obtain latents
@@ -454,3 +323,5 @@ def train(argv):
 
 if __name__ == "__main__":
     app.run(train)
+
+# TODO: Evaluate by passing synthetic hi res latents through decoder
