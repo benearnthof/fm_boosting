43967c5953914bc3c1367d64a3de512efb9671b5
benearnthof <benearnthof@hotmail.de>
2024-08-22 17:55:49 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='fmboosting',
+      version='0.1',
+      description='boosting latent diffusion with flow matching',
+      author='Bene Arnthof',
+      author_email='benearnthof@hotmail.de',
+      url='https://github.com/benearnthof/fm_boosting',
+      packages=find_packages(),
+     )
