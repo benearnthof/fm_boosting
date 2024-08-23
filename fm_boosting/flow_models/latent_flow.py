0386cbe9a5a4daa0f788190b4fe9c0a45a6ea7aa
benearnthof <benearnthof@hotmail.de>
2024-08-23 21:26:34 +0200

    add latent_flow.py

---
+++ fm_boosting/flow_models/latent_flow.py
@@ -0,0 +1,5 @@
+"""
+Short wrapper for latent flow matching, not sure yet if this is needed since technically we can move all the latent stuff into the trainer class.
+"""
+class LatentFlow:
+    pass
